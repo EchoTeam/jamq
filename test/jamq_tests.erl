77d710b377710973cbeffb3d059e661e74917dfc
Yuri Gargay <yuri@aboutecho.com>
2014-02-03 15:45:59 +0400

    F2134: unit test for jamq:delete_queue/2

--- test/jamq_tests.erl
+++ test/jamq_tests.erl
@@ -1,21 +1,24 @@
 -module(jamq_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+-include_lib("amqp_client/include/amqp_client.hrl").
 
 run_test_() ->
     {setup, fun setup/0, fun teardown/1,
          [{"Testing subscribe", fun check_subscribe/0},
-          {"lazy init", {timeout, 30, [fun lazy_init/0]}}]}.
+          {"lazy init", {timeout, 30, [fun lazy_init/0]}},
+          {"Testing delete_queue", fun check_delete_queue/0}]}.
 
 setup() ->
     [application:start(A) || A <- [dht_ring, compiler, syntax_tools, goldrush, lager, jamq]],
-    meck:new(jamq_api),
+    meck:new(jamq_api, [passthrough]),
     meck:expect(jamq_api, start_connection, fun (_) ->  spawn(fun () -> timer:sleep(100000) end) end),
     meck:expect(jamq_api, close_connection, fun (_) -> ok end),
     meck:expect(jamq_api, declare_permanent_exchange, fun (_, _, _) -> ok end),
     meck:expect(jamq_api, close_channel, fun (_) -> ok end),
     meck:expect(jamq_api, publish, fun(_, _, _, _, _) -> ok end),
-    meck:expect(jamq_api, subscribe, fun(_, _, _, _, _, _) -> ok end).
+    meck:expect(jamq_api, subscribe, fun(_, _, _, _, _, _) -> ok end),
+    meck:expect(jamq_api, get_brokers, fun(_) -> ["broker1", "broker2"] end).
 
 teardown(_) ->
     meck:unload().
@@ -48,4 +51,12 @@ lazy_init() ->
     jamq_publisher:stop(role, normal),
     jamq_channel:stop(C, normal).
 
-
+check_delete_queue() ->
+    meck:new(jamq_channel, [passthrough]),
+    meck:expect(jamq_channel, channel, fun(C) -> C end),
+    meck:new(amqp_channel),
+    meck:expect(amqp_channel, call, fun(_, #'queue.delete'{queue = Q}) when is_binary(Q) -> #'queue.delete_ok'{};
+                                    (_, _) -> error end),
+    ?assertEqual(ok, jamq:delete_queue(misc, "tests")),
+    meck:unload(jamq_channel),
+    meck:unload(amqp_channel).
